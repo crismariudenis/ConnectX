9e69520700f7c115f67eafc41fe8bae9487bfcb4
crismariudenis <d-c@seibella.ro>
2021-08-09 18:20:11 +0300

    create url

---
+++ pages/Profile/Parameters.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+export const tagsCode = [
+    backgroundColor= 'bg',
+    dropShadow= 'ds',
+    dropShadowBlur= 'dsblur',
+    dropShadowOffsetY= 'dsyoff',
+    exportSize= 'es',
+    fontFamily= 'fm',
+    fontSize= 'fs',
+    language= 'l',
+    lineHeight= 'lh',
+    lineNumber= 'ln',
+    paddingHorizontal= 'ph',
+    paddingVertical= 'pv',
+    theme= 't',
+    squaredImage= 'si',
+    widthAdjustment= 'wa',
+    windowControl= 'wc',
+    watermark= 'wm',
+    windowTheme= 'wt',
+];
+
+export const BASE_URL = 'https://carbon.now.sh/?';
+
+export const tagsValue = [
+    bg= '#FFFFFF',
+    ds= false,
+    dsblur= '50px',
+    dsyoff= '20px',
+    es= '2x',
+    fm= 'Fira Code',
+    fs= '18px',
+    l= 'auto',
+    lh= '110%',
+    ln= false,
+    pv= '0',
+    ph= '0',
+    t = 'material',
+    si= false,
+    wa= true,
+    wc= true,
+    wt= 'none',
+    wm= false,
+];
+
+
+const styles = StyleSheet.create({})
+
+export default Parameters;
